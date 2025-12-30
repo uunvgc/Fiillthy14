9fbdec2aa8a188811a856bb478a188827fa26d43
uunvgc <Keithross1205@gmail.com>
2025-12-29 23:11:01 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Simple Node.js server
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
